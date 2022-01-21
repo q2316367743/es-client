cc910f27152992f439f61ee381c218982dbc4f72
浅梦凉城 <m17762618644@163.com>
2022-01-21 09:37:45 +0800

    打包路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,16 @@ const path = require('path')
 // import path from 'path'
 
 function _resolve(dir: string) {
-  return path.resolve(__dirname, dir);
+    return path.resolve(__dirname, dir);
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': _resolve('src')
-    }
-  },
-  plugins: [vue()]
+    resolve: {
+        alias: {
+            '@': _resolve('src')
+        }
+    },
+    plugins: [vue()],
+    base: './'
 })
