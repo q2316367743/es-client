0b42b2fb977e1be49746b2ba31bf43d1f4b3ba3d
浅梦凉城 <m17762618644@163.com>
2022-03-02 13:49:51 +0800

    编写验证类

---
+++ src/components/Validate.ts
@@ -0,0 +1,68 @@
+import { ElMessage } from "element-plus";
+
+/**
+ * 验证器
+ */
+export default class Validate {
+
+    /**
+     * 验证状态
+     */
+    public state: boolean;
+
+    constructor() {
+        this.state = true;
+    }
+
+    public valid(): boolean {
+        return this.state;
+    }
+
+    public min(source: number, target: number, message: string): void {
+        if (source < target) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+    }
+
+    public max(source: number, target: number, message: string): void {
+        if (source > target) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+    }
+
+    public range(source: number, min: number, max: number, message: string): void {
+        if (source > max || source < min) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+    }
+
+    public size(source: string | Array<any>, min: number, max: number, message: string): void {
+        if (source.length > max || source.length < min) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+    }
+
+    public notBlank(source: string, message: string): void {
+        if (source === null || source === undefined) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+        source = source.trim();
+        if (source.length === 0) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+    }
+
+    public notNull(source: any, message: string): void {
+        if (source === null || source === undefined) {
+            this.state = false;
+            ElMessage.error(message);
+        }
+    }
+
+}
