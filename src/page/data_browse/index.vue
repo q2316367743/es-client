68016eacc864a6ad450ce7e13ee920cdd9959ddd
浅梦凉城 <m17762618644@163.com>
2022-04-26 14:21:27 +0800

    增加数据浏览模块

---
+++ src/page/data_browse/index.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'data_browse',
+    data: () => ({
+    
+    }),
+});
+</script>
+<style scoped>
+
+</style>
