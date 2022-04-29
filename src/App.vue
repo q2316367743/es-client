41b1034114f093861a2acdb3421017d6f5386eee
浅梦凉城 <m17762618644@163.com>
2022-04-29 10:52:04 +0800

    版本升级

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@
             </el-menu>
             <div class="author">
                 <div style="margin-top: 5px">
-                    <el-link @click="about_dialog = true">v0.8.1</el-link>
+                    <el-link @click="about_dialog = true">v0.9.0</el-link>
                 </div>
             </div>
         </div>
