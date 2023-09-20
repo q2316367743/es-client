41ae8c820addfde8453144dc783535347332b7a4
落雨不悔 <m17762618644@163.com>
2023-09-20 10:48:48 +0800

    修复打印组件问题

--- src/global/BeanFactory.ts
+++ src/global/BeanFactory.ts
@@ -43,7 +43,7 @@ versionStrategyContext.register(new V7VersionStrategyImpl());
 versionStrategyContext.register(new V8VersionStrategyImpl());
 
 // 事件
-export const useExportEvent = useEventBus<ConditionExportEvent>(EventBusEnum.URL_EDIT);
+export const useExportEvent = useEventBus<ConditionExportEvent>(EventBusEnum.CONDITION_EXPORT);
 export const useUrlEditEvent = useEventBus<Url>(EventBusEnum.URL_EDIT);
 export const useIndexManageEvent = useEventBus<string>(EventBusEnum.INDEX_MANAGE);
 export const useSeniorShowResultEvent = useEventBus<void>(EventBusEnum.SENIOR_SHOW_RESULT);
