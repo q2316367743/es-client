bbf98853a7a7ca93eaff5bf1bb1c9d3b398c8a83
落雨不悔 <m17762618644@163.com>
2023-12-13 10:05:55 +0800

    [概览]reindex API

--- src/page/home/components/IndexReindex.tsx
+++ src/page/home/components/IndexReindex.tsx
@@ -1,7 +1,16 @@
 import useIndexStore from "@/store/IndexStore";
 import IndexView from "@/view/index/IndexView";
-import {Button, Modal, Select, Option, TypographyParagraph, Switch, Form, FormItem} from "@arco-design/web-vue";
-import {ref} from "vue";
+import {Button, Modal, Select, Option, Switch, Form, FormItem, ModalReturn} from "@arco-design/web-vue";
+import {Ref, ref} from "vue";
+import MessageUtil from "@/utils/MessageUtil";
+import useLoadingStore from "@/store/LoadingStore";
+import {fetchEs} from "@/plugins/axios";
+import {useSeniorSearchStore} from "@/store/components/SeniorSearchStore";
+
+interface Config {
+    index: string;
+    async: boolean;
+}
 
 /**
  * 索引迁移，从一个索引转到另一个索引
@@ -10,10 +19,10 @@ import {ref} from "vue";
 export function indexReindex(index: string) {
     // 全部的索引
     const indices: Array<IndexView> = useIndexStore().indices;
-    const config = ref({
+    const config = ref<Config>({
         index: '',
         async: false
-    })
+    });
 
     let modalReturn = Modal.open({
         title: `索引【${index}】迁移`,
@@ -26,7 +35,8 @@ export function indexReindex(index: string) {
                 <FormItem label="目标索引">
                     <Select v-model={config.value.index} allowSearch allowClear>
                         {indices.map(item =>
-                            <Option key={item.name} value={item.name}>{item.name}</Option>)}
+                            <Option key={item.name} value={item.name}
+                                    disabled={item.name === index}>{item.name}</Option>)}
                     </Select>
                 </FormItem>
                 <FormItem label="是否异步">
@@ -38,13 +48,44 @@ export function indexReindex(index: string) {
             </Form>
         </>,
         footer: () => <>
-            <Button type="text">跳转到高级查询</Button>
+            <Button type="text" onClick={() => jumpTo(index, config, modalReturn)}>跳转到高级查询</Button>
             <Button onClick={() => modalReturn.close()}>取消</Button>
-            <Button type="primary" onClick={() => modalReturn.close()}>执行</Button>
+            <Button type="primary" onClick={() => onOk(index, config, modalReturn)}>执行</Button>
         </>,
         draggable: true,
-        onOk() {
-            // TODO: 执行
-        }
     });
+}
+
+function jumpTo(index: string, config: Ref<Config>, modalReturn: ModalReturn) {
+    useSeniorSearchStore().loadEvent({
+        method: 'POST',
+        link: '_reindex' + (config.value.async ? '?wait_for_completion=false' : ''),
+        body: `{
+    "source": {"index": "${index}",
+    "dest": {"index": "${config.value.index}"}
+}`
+    });
+    modalReturn.close();
+}
+
+function onOk(index: string, config: Ref<Config>, modalReturn: ModalReturn) {
+    if (config.value.index == '') {
+        MessageUtil.warning("请选择目标索引");
+        return;
+    }
+    useLoadingStore().start("开始进行索引迁移");
+    fetchEs<string>({
+        url: '_reindex' + (config.value.async ? '?wait_for_completion=false' : ''),
+        method: 'POST',
+        data: {
+            source: {index: index},
+            dest: {index: config.value.index}
+        },
+        responseType: 'text'
+    }).then(res => MessageUtil.success(res))
+        .catch(e => MessageUtil.error("迁移失败", e))
+        .finally(() => {
+            useLoadingStore().close();
+            modalReturn.close();
+        })
 }
