f4ebb4569893b5cd5e795edb657b35d7ee267245
落雨不悔 <m17762618644@163.com>
2024-03-17 13:28:38 +0800

    高级查询：载入明细

--- src/page/senior-search/layout/senior-search-editor/components/rest-client-editor/index.vue
+++ src/page/senior-search/layout/senior-search-editor/components/rest-client-editor/index.vue
@@ -1,9 +1,8 @@
 <template>
-    <div ref="container" :id="id" class="es-rest-client-editor"></div>
+    <div ref="container" class="es-rest-client-editor"></div>
 </template>
-<script lang="ts">
-import {defineComponent} from "vue";
-import {mapState} from "pinia";
+<script lang="ts" setup>
+import {computed, onBeforeUnmount, onMounted, ref, watch} from "vue";
 import * as monaco from 'monaco-editor';
 // 语言组件
 import codelens from "./codelens";
@@ -18,139 +17,127 @@ import {useSeniorSearchStore} from "@/store/components/SeniorSearchStore";
 
 let instance: monaco.editor.IStandaloneCodeEditor;
 
-let codeLensProviderDisposable: any | null = null
+let codeLensProviderDisposable: any | null = null;
 
-export default defineComponent({
-    name: 'rest-client-editor',
-    data: () => ({
-        content: '',
-        id: `rest-client-${new Date().getTime()}`,
-        runColor: '#0d7d6c'
-    }),
-    computed: {
-        ...mapState(useGlobalStore, ['isDark'])
-    },
-    watch: {
-        modelValue(newValue) {
-            if (newValue !== instance.getValue()) {
-                instance.setValue(newValue);
-                instance.trigger(instance.getValue(), 'editor.action.formatDocument', null);
-                this.content = newValue;
-            }
+const container = ref<HTMLDivElement>();
+
+const isDark = computed(() => useGlobalStore().isDark);
+const runColor = computed(() => useEditorSettingStore().getRunColor);
+
+watch(() => useSeniorSearchStore().body, value => {
+    if (!instance) {
+        return;
+    }
+    const model = instance.getModel();
+    if (!model) {
+        return;
+    }
+    if (value !== model.getValue()) {
+        model.setValue(value);
+    }
+})
+
+onMounted(() => {
+
+    if (!container.value) {
+        return
+    }
+
+    instance = monaco.editor.create(container.value, {
+        value: useSeniorSearchStore().body,
+        language: 'http',
+        automaticLayout: true,
+        theme: isDark.value ? 'vs-dark' : 'vs',
+        minimap: {
+            enabled: useEditorSettingStore().getMinimap
         },
-        isDark(newValue: boolean) {
-            if (instance) {
-                instance.updateOptions({
-                    theme: newValue ? 'vs-dark' : 'vs'
-                })
+        fontFamily: "consoles",
+        wordWrap: useEditorSettingStore().getWordWrap,
+        fontSize: useEditorSettingStore().getFontSize
+    });
+
+
+    instance.onDidChangeModelContent(() => {
+        const value = instance.getValue();
+        useSeniorSearchStore().updateBody(value);
+        return true;
+    });
+
+    let commandId = instance.addCommand(0, (...args: any[]) => {
+        useSeniorSearchStore().execute(args[1], instance);
+    });
+    // 增加快捷键
+    instance.addCommand(monaco.KeyCode.F9,
+        () => {
+            let value = instance.getValue();
+            let row = Optional.ofNullable(instance.getPosition()).attr("lineNumber").orElse(1);
+            let numbers = renderValue(value);
+            console.log(value, row, numbers)
+            if (numbers.length === 0) {
+                return;
             }
-        }
-    },
-    mounted() {
-
-        const container = this.$refs.container as HTMLElement;
-        instance = monaco.editor.create(container, {
-            value: useSeniorSearchStore().body,
-            language: 'http',
-            automaticLayout: true,
-            theme: this.isDark as boolean ? 'vs-dark' : 'vs',
-            minimap: {
-                enabled: useEditorSettingStore().getMinimap
-            },
-            fontFamily: "consoles",
-            wordWrap: useEditorSettingStore().getWordWrap,
-            fontSize: useEditorSettingStore().getFontSize
-        });
-
-        this.runColor = useEditorSettingStore().getRunColor;
-
-        instance.onDidChangeModelContent(() => {
-            const value = instance.getValue();
-            if (this.content !== value) {
-                useSeniorSearchStore().updateBody(value)
-            }
-            return true;
-        });
-
-        let commandId = instance.addCommand(0, (...args: any[]) => {
-            useSeniorSearchStore().execute(args[1], instance);
-        });
-        // 增加快捷键
-        instance.addCommand(monaco.KeyCode.F9,
-            () => {
-                let value = instance.getValue();
-                let row = Optional.ofNullable(instance.getPosition()).attr("lineNumber").orElse(1);
-                let numbers = this.renderValue(value);
-                console.log(value, row, numbers)
-                if (numbers.length === 0) {
-                    return;
-                }
-                let fail = true;
-                for (let i = 0; i < numbers.length; i++) {
-                    if (numbers[i] > row) {
-                        fail = false;
-                        let target = i - 1;
-                        if (target < 0) {
-                            target = 0;
-                        }
-                        console.log(target)
-                        this.$emit('execute', target);
+            let fail = true;
+            for (let i = 0; i < numbers.length; i++) {
+                if (numbers[i] > row) {
+                    fail = false;
+                    let target = i - 1;
+                    if (target < 0) {
+                        target = 0;
                     }
                 }
-                if (fail) {
-                    useSeniorSearchStore().execute(numbers.length - 1, instance);
+            }
+            if (fail) {
+                useSeniorSearchStore().execute(numbers.length - 1, instance);
 
-                }
-            });
-
-        codeLensProviderDisposable = monaco.languages.registerCodeLensProvider('http', codelens(Optional.ofNullable(commandId).orElse("")));
-
-        // 增加一个动作：格式化文档
-        instance.addAction({
-            // An unique identifier of the contributed action.
-            id: "format rest",
-
-            // A label of the action that will be presented to the user.
-            label: "格式化文档",
-
-            // An optional array of keybindings for the action.
-            keybindings: [],
-
-
-            contextMenuGroupId: "navigation",
-
-            contextMenuOrder: 1.5,
-
-            // Method that will be executed when the action is triggered.
-            // @param editor The editor instance is passed in as a convenience
-            run: function (ed) {
-                ed.setValue(restFormat(ed.getValue()));
-            },
+            }
         });
 
+    codeLensProviderDisposable = monaco.languages.registerCodeLensProvider('http', codelens(Optional.ofNullable(commandId).orElse("")));
 
-    },
-    unmounted() {
-        if (codeLensProviderDisposable) {
-            codeLensProviderDisposable.dispose();
-            codeLensProviderDisposable = null;
-        }
-    },
+    // 增加一个动作：格式化文档
+    instance.addAction({
+        // An unique identifier of the contributed action.
+        id: "format rest",
+
+        // A label of the action that will be presented to the user.
+        label: "格式化文档",
+
+        // An optional array of keybindings for the action.
+        keybindings: [],
 
 
-    methods: {
-        renderValue(value: string): Array<number> {
-            let lineNumbers = new Array<number>();
-            const lines = value.split("\n");
-            for (let i = 0; i < lines.length; i++) {
-                if (lines[i].match(URL_REGEX)) {
-                    lineNumbers.push(i);
-                }
-            }
-            return lineNumbers;
-        }
+        contextMenuGroupId: "navigation",
+
+        contextMenuOrder: 1.5,
+
+        // Method that will be executed when the action is triggered.
+        // @param editor The editor instance is passed in as a convenience
+        run: function (ed) {
+            ed.setValue(restFormat(ed.getValue()));
+        },
+    });
+
+});
+
+onBeforeUnmount(() => {
+    if (codeLensProviderDisposable) {
+        codeLensProviderDisposable.dispose();
+        codeLensProviderDisposable = null;
     }
 });
+
+function renderValue(value: string): Array<number> {
+    let lineNumbers = new Array<number>();
+    const lines = value.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].match(URL_REGEX)) {
+            lineNumbers.push(i);
+        }
+    }
+    return lineNumbers;
+}
+
+
 </script>
 <style lang="less">
 .es-rest-client-editor {
