ebd94d28c2389afb5f1c99eeb39f6d8846cbfa8b
落雨不悔 <m17762618644@163.com>
2025-11-23 23:11:15 +0800

    fix(field-condition): 修复查询字段下拉选项的title显示
    - 为查询字段的a-option添加title属性
    - 解决字段名称无法悬浮显示的问题
    - 提升用户在选择查询字段时的体验
    - 保留选择查询字段组件已有功能和样式

--- src/page/base-search/components/field-condition/item.vue
+++ src/page/base-search/components/field-condition/item.vue
@@ -16,7 +16,7 @@
     <!-- 选择查询字段 -->
     <a-select v-model="condition.field" allow-search allow-create allow-clear placeholder="请选择查询字段"
               style="margin-left: 10px;width: 250px;">
-      <a-option v-for="(field, idx) in fields" :key="idx" :label="field.label" :value="field.name"/>
+      <a-option v-for="(field, idx) in fields" :key="idx" :label="field.label" :value="field.name" :title="field.name"/>
     </a-select>
     <!-- 选择查询条件 -->
     <!-- TODO: 此处还需处理，对于不同类型，需要不同查询条件 -->
